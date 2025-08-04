e3139c22ac7be41e3b1c65f4d3dc660fb96114b8
Najma Razzaq <148422805+najmarazzaq761@users.noreply.github.com>
2025-08-04 10:35:26 +0500

    Time: 0 ms (100%), Space: 18 MB (34.5%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,10 @@
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for ch in ransomNote:
+            if ch in magazine:
+                magazine=magazine.replace(ch,'',1)
+            else:
+                return False
+        return True
+    
