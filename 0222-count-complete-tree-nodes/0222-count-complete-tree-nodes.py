aae246930aa7fafe15d47cdc96245d0db07b3d70
Najma Razzaq <148422805+najmarazzaq761@users.noreply.github.com>
2025-08-06 17:33:48 +0500

    Time: 4 ms (44.43%), Space: 23.3 MB (39.16%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -9,5 +9,24 @@ class Solution:
         if not root:
             return 0
 
+        def getHeightLeft(node):
+            height = 0
+            while node:
+                node = node.left
+                height += 1
+            return height
+
+        def getHeightRight(node):
+            height = 0
+            while node:
+                node = node.right
+                height += 1
+            return height
+
+        left = getHeightLeft(root)
+        right = getHeightRight(root)
+
+        if left == right:
+            return (1 << left) - 1  # 2^left - 1
         else:
             return 1 + self.countNodes(root.left) + self.countNodes(root.right)
