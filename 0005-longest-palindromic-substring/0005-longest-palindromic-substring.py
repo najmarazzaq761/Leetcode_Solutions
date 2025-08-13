df0528d5d57ed16bfe6ce0f604c9baf1292f8c0f
Najma Razzaq <148422805+najmarazzaq761@users.noreply.github.com>
2025-08-13 07:56:22 +0500

    Time: 260 ms (74.56%), Space: 18 MB (25.85%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -10,11 +10,11 @@ class Solution:
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 left -= 1
                 right += 1
-            return right - left - 1  # length of palindrome
+            return right - left - 1  
         
         for i in range(len(s)):
-            len1 = expandAroundCenter(i, i)       # Odd length palindrome
-            len2 = expandAroundCenter(i, i + 1)   # Even length palindrome
+            len1 = expandAroundCenter(i, i)       
+            len2 = expandAroundCenter(i, i + 1)   
             max_len = max(len1, len2)
             
             if max_len > (end - start):
