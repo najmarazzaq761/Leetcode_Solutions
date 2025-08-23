e74c0eb3f9e8fed38cc34809dba9d2a01032be8a
Najma Razzaq <148422805+najmarazzaq761@users.noreply.github.com>
2025-08-23 09:36:11 +0500

    Time: 107 ms (88.65%), Space: 77.9 MB (92.39%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,56 +1,23 @@
-class Node:
-    def __init__(self, key: int, value: int):
-        self.key = key
-        self.value = value
-        self.prev = None
-        self.next = None
+from collections import OrderedDict
 
 class LRUCache:
+
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = {}   # key -> node
-
-        # Dummy head & tail to avoid edge cases
-        self.head = Node(0, 0)
-        self.tail = Node(0, 0)
-        self.head.next = self.tail
-        self.tail.prev = self.head
-
-    # helper: remove node
-    def _remove(self, node: Node):
-        prev, nxt = node.prev, node.next
-        prev.next, nxt.prev = nxt, prev
-
-    # helper: insert node right after head (MRU position)
-    def _add(self, node: Node):
-        node.next = self.head.next
-        node.prev = self.head
-        self.head.next.prev = node
-        self.head.next = node
+        self.cache = OrderedDict()
 
     def get(self, key: int) -> int:
-        if key in self.cache:
-            node = self.cache[key]
-            self._remove(node)   # move to head
-            self._add(node)
-            return node.value
-        return -1
+        if key not in self.cache:
+            return -1
+        # move accessed key to the end (most recently used)
+        self.cache.move_to_end(key)
+        return self.cache[key]
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self._remove(self.cache[key])  # remove old node
-        node = Node(key, value)
-        self._add(node)
-        self.cache[key] = node
-
+            # update existing key and move to end
+            self.cache.move_to_end(key)
+        self.cache[key] = value
         if len(self.cache) > self.capacity:
-            # remove from tail
-            lru = self.tail.prev
-            self._remove(lru)
-            del self.cache[lru.key]
-
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+            # pop the least recently used (first item)
+            self.cache.popitem(last=False)
