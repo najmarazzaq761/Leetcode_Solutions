5c1eee2ada39a8fec98fb35e96de8b49a3ed14a6
Najma Razzaq <148422805+najmarazzaq761@users.noreply.github.com>
2025-09-12 09:31:33 +0500

    Time: 0 ms (100%), Space: 18.9 MB (93.73%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -2,38 +2,43 @@ class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         first_occurance = -1
         last_occurance = -1
-        # left, right = 0,len(nums)-1
-        # while left <= right:
-        #     mid = (left+right)//2
-        #     if target == nums[mid]:
-        #         first_occurance = mid
-        #         right = mid -1
-        #     elif target < nums[mid]:
-        #         right = mid -1
-        #     else:
-        #         left = mid + 1
+        left = 0
+        right = len(nums)-1
+        while left<=right:
+            mid = (left+right)//2
+            if target == nums[mid]:
+                first_occurance = mid
+                right = mid -1
+            elif target < nums[mid]:
+                right = mid -1
+            else:
+                left = mid+1
+        
+        left = 0
+        right = len(nums)-1
 
-
-        # left , right = 0, len(nums)-1
-        # while left <= right:
-        #     mid = (left+right)//2
-        #     if target == nums[mid]:
-        #         last_occurance = mid
-        #         left = mid + 1
-        #     elif target < nums[mid]:
-        #         right = mid -1
-        #     else:
-        #         left = mid + 1
-
-        # return [first_occurance, last_occurance]
-        for i in range(len(nums)):
-            if nums[i] == target:
-                first_occurance = i
-                break
-
-        for i in range(len(nums)-1, -1, -1):
-            if nums[i] == target:
-                last_occurance = i
-                break
+        while left<=right:
+            mid = (left+right)//2
+            if target == nums[mid]:
+                last_occurance = mid
+                left = mid +1
+            elif target < nums[mid]:
+                right = mid -1
+            else:
+                left = mid+1
+        
         return [first_occurance, last_occurance]
+
+        
+
+        # for i in range(len(nums)):
+        #     if nums[i] == target:
+        #         first_occurance = i
+        #         break
+
+        # for i in range(len(nums)-1, -1, -1):
+        #     if nums[i] == target:
+        #         last_occurance = i
+        #         break
+        
             
