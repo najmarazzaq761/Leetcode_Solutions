d00913c61618c6cdae48bff72abe900809cd75a5
Najma Razzaq <148422805+najmarazzaq761@users.noreply.github.com>
2025-09-12 18:16:21 +0500

    Time: 0 ms (100%), Space: 18 MB (21.17%) - LeetHub

--- 0086-partition-list/0086-partition-list.py
+++ 0086-partition-list/0086-partition-list.py
@@ -5,18 +5,18 @@
 #         self.next = next
 class Solution: 
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
-        before_head = ListNode(0)
-        after_head = ListNode(0)
-        before = before_head
-        after = after_head
+        lesser = ListNode(0)
+        greater_equal = ListNode(0)
+        i = lesser
+        j = greater_equal
         while head:
-            if head.val < x:
-                before.next = head
-                before = before.next
+            if head.val < x:          
+                i.next = head
+                i = i.next
             else:
-                after.next = head
-                after = after.next
+                j.next = head
+                j = j.next
             head = head.next
-        after.next = None
-        before.next = after_head.next
-        return before_head.next
+        j.next = None
+        i.next = greater_equal.next
+        return lesser.next
