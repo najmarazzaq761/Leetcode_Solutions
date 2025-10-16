999d4c331ffa464cb6727df99ca5f3753234ebde
Najma Razzaq <148422805+najmarazzaq761@users.noreply.github.com>
2025-10-16 21:58:58 +0500

    Time: 3 ms (64.59%), Space: 19.6 MB (77.08%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,28 +1,8 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        # nums.sort()
-        # for i in range(0, len(nums) - 1, 2):
-        #     if nums[i] != nums[i + 1]:
-        #         return nums[i]
-        # return nums[-1]
-
-
-        # for i in range(len(nums)):
-        #     count = 0
-        #     for j in range(len(nums)):
-        #         if nums[i] == nums[j]:
-        #             count += 1
-        #     if count == 1:
-        #         return nums[i]
-
-
-        # result=0
-        # for num in nums:
-        #     result ^= num
-        # return result
-
-
-        count = Counter(nums)
-        for num, freq in count.items():
-            if freq == 1:
-                return num
+        nums.sort()
+        for i in range(0, len(nums)-1, 2):
+            if nums[i] != nums[i+1]:
+                return nums[i]
+        return nums[-1]
+        
