e390d9e7285ed091d54dc5f9df9cbda606c543c7
Najma Razzaq <148422805+najmarazzaq761@users.noreply.github.com>
2025-10-28 20:39:03 +0500

    Time: 0 ms (100%), Space: 18 MB (64.6%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,16 +1,51 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        result = []
-        for num in nums1:
-            found = False
-            for i in range(len(nums2)):
-                if nums2[i] == num:
-                    for j in range(i + 1, len(nums2)):
-                        if nums2[j] > num:
-                            result.append(nums2[j])
-                            found = True
-                            break
-                    if not found:
-                        result.append(-1)
-                    break
-        return result
+        stack = []              # Monotonic decreasing stack
+        next_greater = {}       # HashMap: num -> next greater element
+
+        # Traverse nums2 and fill next_greater
+        for num in nums2:
+            # While current num is greater than stack's top, pop and assign mapping
+            while stack and num > stack[-1]:
+                smaller = stack.pop()
+                next_greater[smaller] = num
+            stack.append(num)
+
+        # Remaining elements in stack have no next greater element
+        for num in stack:
+            next_greater[num] = -1
+
+        # Build the result for nums1 using the hashmap
+        return [next_greater[num] for num in nums1]
+
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+        # result = []
+        # for num in nums1:
+        #     found = False
+        #     for i in range(len(nums2)):
+        #         if nums2[i] == num:
+        #             for j in range(i + 1, len(nums2)):
+        #                 if nums2[j] > num:
+        #                     result.append(nums2[j])
+        #                     found = True
+        #                     break
+        #             if not found:
+        #                 result.append(-1)
+        #             break
+        # return result
