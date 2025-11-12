e4ca3b708427cbc6b81bfa6c71cd34d7dc28da27
Najma Razzaq <148422805+najmarazzaq761@users.noreply.github.com>
2025-11-12 22:03:37 +0500

    Time: 0 ms (100%), Space: 19.3 MB (44.2%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.py
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.py
@@ -7,7 +7,7 @@
 class Solution:
     def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:         
 
-        while root:
+        if root:
             if root.val == val:
                 return root
             elif root.val > val:
