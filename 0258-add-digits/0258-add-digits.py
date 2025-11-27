5dcd5ca080aca805e4e854b1c6270d99ba8947ab
Najma Razzaq <148422805+najmarazzaq761@users.noreply.github.com>
2025-11-27 11:33:49 +0500

    Time: 0 ms (100%), Space: 17.9 MB (36.15%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -1,11 +1,17 @@
 class Solution:
     def addDigits(self, num: int) -> int:
+        while num > 9:
+             rem = num % 10
+             qt = num // 10
+             num = rem + qt 
+        return num
+
+ # separate and add those two numbers then again compare if still greater then 9 then add again 
         
-        
-        while num >=10:
-            res = 0
-            while num > 0:
-                res += num % 10
-                num //= 10
-            num = res
-        return num    
+        # while num >=10:
+        #     res = 0
+        #     while num > 0:
+        #         res += num % 10
+        #         num //= 10
+        #     num = res
+        # return num    
