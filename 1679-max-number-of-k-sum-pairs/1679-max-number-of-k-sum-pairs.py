4330ba4b61246a4d4b574fc9756f8dad3c29f499
Najma Razzaq <148422805+najmarazzaq761@users.noreply.github.com>
2025-12-27 20:24:18 +0500

    Time: 473 ms (98.08%), Space: 30.1 MB (49.82%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
@@ -1,18 +1,33 @@
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
-        nums.sort()
-        i = 0
+        # nums.sort()
+        # i = 0
+        # count = 0
+        # j = len(nums) - 1
+        # while i < j:
+        #     if (nums[i] + nums[j]) == k:
+        #         i+=1
+        #         j-=1
+        #         count += 1
+        #     elif nums[i] + nums[j] < k:
+        #         i+=1
+        #     else:
+        #         j-=1
+        # return count
+       
         count = 0
-        j = len(nums) - 1
-        while i < j:
-            if (nums[i] + nums[j]) == k:
-                i+=1
-                j-=1
+        freq = {}
+
+        for num in nums:
+            complement = k - num
+
+            if freq.get(complement,0) > 0:
                 count += 1
-            elif nums[i] + nums[j] < k:
-                i+=1
+                freq[complement] -= 1
             else:
-                j-=1
+                freq[num] = freq.get(num, 0) + 1
+
         return count
+
         
             
