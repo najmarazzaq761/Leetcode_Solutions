a932baeba4a68fc5b9ded5a4f8e6ed6289543adc
Najma Razzaq <148422805+najmarazzaq761@users.noreply.github.com>
2025-12-28 17:30:31 +0500

    Time: 68 ms (5.8%), Space: 17.6 MB (89.28%) - LeetHub

--- 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.py
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.py
@@ -1,6 +1,39 @@
 class Solution:
     def minimumDifference(self, nums: List[int], k: int) -> int:
-        nums.sort()
+        # nums.sort()
+           # bubble sort
+        # for i in range(len(nums)-1):
+        #     for j in range(i+1,len(nums)):
+        #         if nums[j] < nums[i]:
+        #             nums[i], nums[j] = nums[j], nums[i]
+
+              # merge sort
+        def merge_sort(nums):
+            if len(nums) <= 1:
+                return nums
+
+            mid = len(nums) // 2
+            left = merge_sort(nums[:mid])
+            right = merge_sort(nums[mid:])
+
+            return merge(left, right)
+
+        def merge(left, right):
+            result = []
+            i = j = 0
+
+            while i < len(left) and j < len(right):
+                if left[i] <= right[j]:
+                    result.append(left[i])
+                    i += 1
+                else:
+                    result.append(right[j])
+                    j += 1
+            result.extend(left[i:])
+            result.extend(right[j:])
+            return result        
+
+        nums = merge_sort(nums)
         l = 0
         r = k - 1
         n = len(nums)
