c41285a219ac73a767419f8f92f8a5b70c7cc14f
Najma Razzaq <148422805+najmarazzaq761@users.noreply.github.com>
2026-09-21 23:10:59 +0500

    Time: 5 ms (2.92%), Space: 12.4 MB (84.09%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -1,19 +1,27 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if not list1:
-            return list2
-        if not list2:
-            return list1
-        elif list1.val<=list2.val:
-            list1.next=self.mergeTwoLists(list1.next, list2)
-            return list1
-        else:
-            list2.next=self.mergeTwoLists(list2.next, list1)
-            return list2
-            
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        dummy = ListNode()
+        current = dummy
+        while list1 and list2:
+            if list1.val <= list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+
+            current = current.next
+
+        if list1:
+            current.next = list1
+        if list2:
+            current.next = list2
+        return dummy.next
+
+
         
